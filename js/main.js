b1c727f995f3f77424dc2de03a1dd9bba9247ffa
Joe Cowman <joe.cowman@huskers.unl.edu>
2017-11-05 14:51:29 -0600

    Placeholder for dropdown

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
-  $(document.body).append("Startup successful.<br>");
-  load();
+
+  var $getGroupBtn = $('#getGroupBtn');
+  var $tokenInput = $('#tokenInput');
+  var $groupSelect = $('#groupSelect');
+  var $selectGroupBtn = $('#selectGroupBtn');
+  $getGroupBtn.click(function() {
+    $getGroupBtn.hide();
+    $tokenInput.attr("readonly", "readonly");
+    $groupSelect.show();
+    $selectGroupBtn.show();
+  });
+  //$(document.body).append("Startup successful.<br>");
+  //load();
 });
